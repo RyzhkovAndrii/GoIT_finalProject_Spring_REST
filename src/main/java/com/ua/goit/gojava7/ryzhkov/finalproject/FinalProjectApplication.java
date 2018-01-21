8175e08b1d8692ed111514d2bd9a6276b99ae2c6
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-01-21 11:19:25 +0200

    renamed application

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/FinalProjectApplication.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/FinalProjectApplication.java
@@ -2,11 +2,13 @@ package com.ua.goit.gojava7.ryzhkov.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @SpringBootApplication
-public class FinalprojectApplication {
+@IntegrationComponentScan
+public class FinalProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalprojectApplication.class, args);
+		SpringApplication.run(FinalProjectApplication.class, args);
 	}
 }

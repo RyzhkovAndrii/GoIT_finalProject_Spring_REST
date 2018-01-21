ab227f89821bf8f456d3966425b2d0ca1f93a268
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-01-21 11:43:04 +0200

    all methods of role controller were added

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/RoleController.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/RoleController.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/roles")
@@ -20,8 +19,29 @@ public class RoleController {
     private RoleService roleService;
 
     @RequestMapping(method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Collection<Role>> list() {
+    public ResponseEntity<Collection<Role>> getListRoles() {
         return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Role> getRole(@PathVariable("id") UUID id) {
+        Role role = roleService.findById(id);
+        return role != null
+                ? new ResponseEntity<>(role, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        return new ResponseEntity<>(roleService.save(role), HttpStatus.CREATED);
+    }
+
+    // todo put method
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Void> deleteRole(@PathVariable("id") UUID id) {
+        roleService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }

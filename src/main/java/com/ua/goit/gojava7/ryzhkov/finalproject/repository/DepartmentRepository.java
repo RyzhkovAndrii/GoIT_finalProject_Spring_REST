4f9716515d5cf4e3d5c3b169065c05f455b3a84f
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-01-21 19:32:39 +0200

    @Repository annotation was added

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/repository/DepartmentRepository.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/repository/DepartmentRepository.java
@@ -1,8 +1,10 @@
 package com.ua.goit.gojava7.ryzhkov.finalproject.repository;
 
 import com.ua.goit.gojava7.ryzhkov.finalproject.model.Department;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface DepartmentRepository extends NamedEntityRepository<Department, UUID> {
 }
--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/repository/EmployeePostRepository.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/repository/EmployeePostRepository.java
@@ -1,8 +1,10 @@
 package com.ua.goit.gojava7.ryzhkov.finalproject.repository;
 
 import com.ua.goit.gojava7.ryzhkov.finalproject.model.EmployeePost;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EmployeePostRepository extends NamedEntityRepository<EmployeePost, UUID> {
 }

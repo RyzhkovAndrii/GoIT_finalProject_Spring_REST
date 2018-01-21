b89e6ae24bd3b816b9c990354e9e57b34d9b2d5d
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-01-21 15:13:31 +0200

    was made extends from base and named generic controllers

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/RoleController.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/RoleController.java
@@ -3,45 +3,18 @@ package com.ua.goit.gojava7.ryzhkov.finalproject.rest;
 import com.ua.goit.gojava7.ryzhkov.finalproject.model.Role;
 import com.ua.goit.gojava7.ryzhkov.finalproject.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/roles")
-public class RoleController {
+public class RoleController extends NamedEntityController<Role, UUID> {
 
     @Autowired
-    private RoleService roleService;
-
-    @RequestMapping(method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Collection<Role>> getListRoles() {
-        return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Role> getRole(@PathVariable("id") UUID id) {
-        Role role = roleService.findById(id);
-        return role != null
-                ? new ResponseEntity<>(role, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
-        return new ResponseEntity<>(roleService.save(role), HttpStatus.CREATED);
-    }
-
-    // todo put method
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Void> deleteRole(@PathVariable("id") UUID id) {
-        roleService.delete(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public RoleController(RoleService roleService) {
+        super(roleService);
     }
 
 }
--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/UserController.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/UserController.java
@@ -3,45 +3,18 @@ package com.ua.goit.gojava7.ryzhkov.finalproject.rest;
 import com.ua.goit.gojava7.ryzhkov.finalproject.model.User;
 import com.ua.goit.gojava7.ryzhkov.finalproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserController {
+public class UserController extends BaseEntityController<User, UUID> {
 
     @Autowired
-    private UserService userService;
-
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Collection<User>> getListUsers() {
-        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<User> getUser(@PathVariable("id") UUID id) {
-        User user = userService.findById(id);
-        return user != null
-                ? new ResponseEntity<>(user, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
-        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
-    }
-
-    // todo put method
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Void> deleteUser(@PathVariable("id") UUID id) {
-        userService.delete(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public UserController(UserService userService) {
+        super(userService);
     }
 
 }

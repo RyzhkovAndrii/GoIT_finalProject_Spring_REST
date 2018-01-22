e3540a5a5ba23a1c284eae8821d353b71133cc51
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-01-22 23:11:02 +0200

    not null property was added to fields, custom serializer and deserializer were added

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/model/User.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/model/User.java
@@ -1,7 +1,9 @@
 package com.ua.goit.gojava7.ryzhkov.finalproject.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ua.goit.gojava7.ryzhkov.finalproject.util.deserializer.CustomUserDeserializer;
+import com.ua.goit.gojava7.ryzhkov.finalproject.util.serializer.CustomUserSerializer;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -19,34 +21,32 @@ import java.util.Set;
 @Setter
 @RequiredArgsConstructor
 @ToString
-@JsonIgnoreProperties(value = "employee", allowGetters = true)
+@JsonSerialize(using = CustomUserSerializer.class)
+@JsonDeserialize(using = CustomUserDeserializer.class)
 public class User extends BaseEntity {
 
-    @Column(name = "username")
+    @Column(name = "username", nullable = false)
     private String username;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
 
     @Transient
     private String confirmPassword;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "registration_date")
+    @Column(name = "registration_date", nullable = false)
     private Date registrationDate;
 
     @ManyToMany
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-    @JsonIgnore
     private Set<Role> roles;
 
     @OneToOne(mappedBy = "user")
-    @JsonIgnore
     private Employee employee;
 
-    @JsonIgnore
     public Set<Role> getSetRoles() {
         return roles;
     }

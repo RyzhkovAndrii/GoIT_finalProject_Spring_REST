c4fb03de7e8c4a5ac3cc801de43095dd1bc97b1d
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-01-25 13:01:47 +0200

    security configurations were changed

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/config/SecurityConfig.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.ua.goit.gojava7.ryzhkov.finalproject.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,22 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-                .ignoring()
-                .antMatchers("/h2/**")
-                .antMatchers("/swagger-ui.html/**");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .and()
-                .csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated();
+        http.csrf().disable();
+        http.formLogin();
     }
 
 }

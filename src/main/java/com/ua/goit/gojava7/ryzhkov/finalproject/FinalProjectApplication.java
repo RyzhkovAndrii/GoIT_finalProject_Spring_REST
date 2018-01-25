e236345e903efa6dad5d30707310e2f4e6fe5f62
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-01-25 12:58:58 +0200

    @EnableScheduling was added

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/FinalProjectApplication.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/FinalProjectApplication.java
@@ -3,9 +3,11 @@ package com.ua.goit.gojava7.ryzhkov.finalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @IntegrationComponentScan
+@EnableScheduling
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {

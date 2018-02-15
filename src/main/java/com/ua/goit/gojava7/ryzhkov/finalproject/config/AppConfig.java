d2b6447c7c8d0b20cba2122ab6a7d822afdb1994
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-02-15 13:18:09 +0200

    modelmapper was added

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/config/AppConfig.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.ua.goit.gojava7.ryzhkov.finalproject.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -7,16 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class AppConfig extends WebMvcConfigurerAdapter {
 
-    /*@Bean
-    public Jackson2JsonObjectMapper jackson2JsonObjectMapper() {
-        return ObjectMapperFactory.getMapper();
-    }*/
-
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedOrigins("*");
     } // todo what is it
+
+    @Bean
+    public ModelMapper modelMapper() { // todo where must it be?
+        return new ModelMapper();
+    }
 
 }

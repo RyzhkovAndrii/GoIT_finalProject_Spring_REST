5b57b55a40939f29e7f102d6a7db072e400956d6
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-02-21 16:39:33 +0200

    EmployeeStatusDto was added ResponseEntity was changed on ResponseBody

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/EmployeeStatusController.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/EmployeeStatusController.java
@@ -1,17 +1,20 @@
 package com.ua.goit.gojava7.ryzhkov.finalproject.rest;
 
 import com.ua.goit.gojava7.ryzhkov.finalproject.model.EmployeeStatus;
+import com.ua.goit.gojava7.ryzhkov.finalproject.model.dto.EmployeeStatusDto;
+import com.ua.goit.gojava7.ryzhkov.finalproject.model.dto.EmployeeStatusSaveDto;
 import com.ua.goit.gojava7.ryzhkov.finalproject.service.EmployeeStatusService;
 import io.swagger.annotations.ApiOperation;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employee-statuses")
@@ -20,49 +23,72 @@ public class EmployeeStatusController {
 
     private EmployeeStatusService employeeStatusService;
 
+    private ModelMapper modelMapper;
+
     @Autowired
-    public EmployeeStatusController(EmployeeStatusService employeeStatusService) {
+    public EmployeeStatusController(EmployeeStatusService employeeStatusService, ModelMapper modelMapper) {
         this.employeeStatusService = employeeStatusService;
+        this.modelMapper = modelMapper;
     }
 
-    @ApiOperation(value = "view list of employee statuses", response = Collection.class)
+    @ApiOperation(value = "view list of employee statuses")
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Collection<EmployeeStatus>> getList() {
-        return new ResponseEntity<>(employeeStatusService.findAll(), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Collection<EmployeeStatusDto> getList() {
+        return convertToDto(employeeStatusService.findAll());
     }
 
-    @ApiOperation(value = "search employee status with name", response = EmployeeStatus.class)
-    // todo same ulr like list
+    @ApiOperation(value = "search employee status with name") // todo same ulr like list
     @RequestMapping(params = "name", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<EmployeeStatus> getByName(@RequestParam String name) {
-        return new ResponseEntity<>(employeeStatusService.findByName(name), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public EmployeeStatusDto getByName(@RequestParam String name) {
+        return convertToDto(employeeStatusService.findByName(name));
     }
 
-    @ApiOperation(value = "search employee status with ID", response = EmployeeStatus.class)
+    @ApiOperation(value = "search employee status with ID")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<EmployeeStatus> get(@PathVariable UUID id) {
-        return new ResponseEntity<>(employeeStatusService.findById(id), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public EmployeeStatusDto get(@PathVariable UUID id) {
+        return convertToDto(employeeStatusService.findById(id));
     }
 
-    @ApiOperation(value = "add employee status", response = EmployeeStatus.class)
+    @ApiOperation(value = "add employee status")
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<EmployeeStatus> save(@RequestBody EmployeeStatus employeeStatus) {
-        return new ResponseEntity<>(employeeStatusService.save(employeeStatus), HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public EmployeeStatusDto save(@RequestBody EmployeeStatusSaveDto employeeStatusDto) {
+        return convertToDto(employeeStatusService.save(convertToEntity(employeeStatusDto)));
     }
 
     @ApiOperation(value = "update employee status")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Void> update(@PathVariable UUID id, @RequestBody EmployeeStatus employeeStatus) {
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@PathVariable UUID id, @RequestBody EmployeeStatusSaveDto employeeStatusDto) {
+        EmployeeStatus employeeStatus = convertToEntity(employeeStatusDto);
         employeeStatus.setId(id);
-        employeeStatusService.save(employeeStatus);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        employeeStatusService.update(employeeStatus);
     }
 
     @ApiOperation(value = "delete employee status")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable UUID id) {
         employeeStatusService.delete(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private EmployeeStatus convertToEntity(EmployeeStatusSaveDto dto) {
+        return modelMapper.map(dto, EmployeeStatus.class);
+    }
+
+    private EmployeeStatusDto convertToDto(EmployeeStatus employeeStatus) {
+        return modelMapper.map(employeeStatus, EmployeeStatusDto.class);
+    }
+
+    private Collection<EmployeeStatusDto> convertToDto(Collection<EmployeeStatus> employeeStatuses) {
+        return employeeStatuses.stream().map(this::convertToDto).collect(Collectors.toList());
     }
 
 }

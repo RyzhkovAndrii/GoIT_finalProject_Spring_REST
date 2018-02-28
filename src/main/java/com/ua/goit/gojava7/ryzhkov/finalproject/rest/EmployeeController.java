56e8ab69c1bd8d817919e8c0bc257bae8cb97177
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-02-28 16:40:37 +0200

    ResponseEntity was changed on ResponseBody dto and conversion service was added

--- src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/EmployeeController.java
+++ src/main/java/com/ua/goit/gojava7/ryzhkov/finalproject/rest/EmployeeController.java
@@ -1,14 +1,14 @@
 package com.ua.goit.gojava7.ryzhkov.finalproject.rest;
 
+import com.ua.goit.gojava7.ryzhkov.finalproject.converter.ModelConversionService;
+import com.ua.goit.gojava7.ryzhkov.finalproject.dto.EmployeeRequest;
+import com.ua.goit.gojava7.ryzhkov.finalproject.dto.EmployeeResponse;
 import com.ua.goit.gojava7.ryzhkov.finalproject.model.Employee;
 import com.ua.goit.gojava7.ryzhkov.finalproject.service.EmployeeService;
-import com.ua.goit.gojava7.ryzhkov.finalproject.service.UserService;
 import io.swagger.annotations.ApiOperation;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,65 +22,52 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
-    private UserService userService;
-
-    private ModelMapper modelMapper;
+    private ModelConversionService conversionService;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService, ModelMapper modelMapper) {
+    public EmployeeController(EmployeeService employeeService, ModelConversionService conversionService) {
         this.employeeService = employeeService;
-        this.modelMapper = modelMapper;
+        this.conversionService = conversionService;
     }
 
-    @ApiOperation(value = "view list of employees", response = Collection.class)
+    @ApiOperation(value = "view list of employees")
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Collection<Employee>> getList() {
-        return new ResponseEntity<>(employeeService.findAll(), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public Collection<EmployeeResponse> getList() {
+        return conversionService.convert(employeeService.findAll(), EmployeeResponse.class);
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE USER')")
 //            "or #id == @securityServiceImpl.findLoggedInEmployeeId()") // todo not working
-    @ApiOperation(value = "search employee with ID", response = Employee.class)
+    @ApiOperation(value = "search employee with ID")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Employee> get(@PathVariable UUID id) {
-        return new ResponseEntity<>(employeeService.findById(id), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public EmployeeResponse get(@PathVariable UUID id) {
+        return conversionService.convert(employeeService.findById(id), EmployeeResponse.class);
     }
 
     @ApiOperation(value = "add employee", response = Employee.class)
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Employee> save(@RequestBody Employee employee) {
-        return new ResponseEntity<>(employeeService.save(employee), HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public EmployeeResponse save(@RequestBody EmployeeRequest employeeRequest) {
+        Employee employee = conversionService.convert(employeeRequest, Employee.class);
+        return conversionService.convert(employeeService.save(employee), EmployeeResponse.class);
     }
 
     @ApiOperation(value = "update employee")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Void> update(@PathVariable UUID id, @RequestBody Employee employee) {
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@PathVariable UUID id, @RequestBody EmployeeRequest employeeRequest) {
+        Employee employee = conversionService.convert(employeeRequest, Employee.class);
         employee.setId(id);
         employeeService.save(employee);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @ApiOperation(value = "delete employee")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable UUID id) {
         employeeService.delete(id);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
-
-    /*private Employee convertToEntity(EmployeeSaveDto dto) {
-        Employee employee = modelMapper.map(dto, Employee.class);
-        if (dto.getUserId() != null) {
-            employee.setUser(userService.findById(dto.getUserId()));
-        }
-        return employee;
-    }
-
-    private EmployeeDto convertToDto(Employee employee) {
-        return modelMapper.map(employee, EmployeeDto.class);
-    }
-
-    private Collection<EmployeeDto> convertToDto(Collection<Employee> employees) {
-        return employees.stream().map(this::convertToDto).collect(Collectors.toList());
-    }*/
 
 }
